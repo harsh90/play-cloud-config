730518e5a03766f856d919deb4bb33e38cd02391
De Silva, Harshitha <harshitha.desilva@pearson.com>
2018-10-11 15:10:18 +0530

    Initilize the project

---
+++ src/main/java/com/playground/configserver/playgroudconfig/PlayGroudConfigApplication.java
@@ -0,0 +1,14 @@
+package com.playground.configserver.playgroudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class PlayGroudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayGroudConfigApplication.class, args);
+	}
+}
---
+++ src/test/java/com/playground/configserver/playgroudconfig/PlayGroudConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.playground.configserver.playgroudconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlayGroudConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
